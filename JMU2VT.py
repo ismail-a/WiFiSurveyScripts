dec4c326b0e5dafc0f985c191c26cd74e7bfdba1
Ismail Arai <ismail0311@gmail.com>
2020-04-16 21:56:55 -0400

    add converting scripts from JMU format to VT format

---
+++ JMU2VT.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This script converts an JMU's CSV to a VT's CSV.
+Usage:
+% ./JMU2VT.py JMUCSV.csv > VTCSV.csv
+"""
+
+import sys
+import csv
+
+def channel2freq(channel):
+    if 1 <= channel <= 13:
+        return 2407 + channel * 5
+    elif channel == 14:
+        return 2484
+    elif 36 <= channel <= 165:
+        return 5000 + channel * 5
+    else:
+        return 0
+
+args = sys.argv
+wifi_file = open(args[1], "r")
+wifi_csv = csv.DictReader(wifi_file, delimiter = ",")
+
+aps = {}
+index = 0
+for beacon in wifi_csv:
+    if beacon['bssid'] in aps:
+        continue
+    aps[beacon['bssid']] = {'channel': beacon['channel'], 'ssid': beacon['ssid'], 'index': index}
+    index += 1
+
+headers = ["Channel,", "SSID,", "time,"]
+
+for key in aps:
+    headers[0] += str(channel2freq(int(aps[key]['channel']))) + ","
+    headers[1] += aps[key]['ssid'] + ","
+    headers[2] += key + ","
+headers[2] += "loc"
+
+for header in headers:
+    print(header)
+
+wifi_file.seek(0)
+wifi_csv = csv.DictReader(wifi_file, delimiter = ",")
+for beacon in wifi_csv:
+    line = beacon['datetime'] + ","
+    for i in range(aps[beacon['bssid']]['index']):
+        line += "0,"
+    line += beacon['rssi'] + ","
+    for i in range(len(aps) - aps[beacon['bssid']]['index'] - 1):
+        line += "0,"
+    line += beacon['point_id']
+    print(line)
---
+++ separateCSVbyRoomCategory.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This script separates JMU's CSV to 3 category of Room, Operation-room, and Hallway.
+Usage:
+% ./separateCSVbyRoomCategory.py JMUCSV.csv point_loc.csv
+Output:
+./JMUCSV-hallways-separated.csv
+./JMUCSV-operations-separated.csv
+./JMUCSV-rooms-separated.csv
+"""
+
+import sys
+import csv
+
+args = sys.argv
+
+point_loc_file = open(args[2], "r")
+point_loc_reader = csv.DictReader(point_loc_file)
+point_loc_dict = {}
+for row in point_loc_reader:
+    point_loc_dict[row['point_id']] = row['loc_id']
+point_loc_file.close()
+
+csv_file = open(args[1], "r")
+csv_file_reader = csv.reader(csv_file)
+header = next(csv_file_reader)
+csv_hallways_file = open(args[1][:-4] + "-hallwars-separated.csv", "w")
+csv_hallways_writer = csv.writer(csv_hallways_file)
+csv_hallways_writer.writerow(header)
+csv_operations_file = open(args[1][:-4] + "-operations-separated.csv", "w")
+csv_operations_writer = csv.writer(csv_operations_file)
+csv_operations_writer.writerow(header)
+csv_rooms_file = open(args[1][:-4] + "-rooms-separated.csv", "w")
+csv_rooms_writer = csv.writer(csv_rooms_file)
+csv_rooms_writer.writerow(header)
+
+for row in csv_file_reader:
+    row[0] = point_loc_dict[row[0]]
+    category = int(row[0]) // 100
+    if category == 1:
+        csv_hallways_writer.writerow(row)
+    elif category == 2:
+        csv_rooms_writer.writerow(row)
+    elif category == 3:
+        csv_operations_writer.writerow(row)
+
+csv_file.close()
+csv_hallways_file.close()
+csv_operations_file.close()
+csv_rooms_file.close()
